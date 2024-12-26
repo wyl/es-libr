cf8784bbae0588d2e6aa743d284f40a28576ba0c
Wang yalong <v1wangyalong@sph.com.sg>
2024-12-26 14:10:27 +0800

    fix: add /dest to .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
+  // { ignores: ['dist/*'] },
+  js.configs.recommended,
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {},
 ]
